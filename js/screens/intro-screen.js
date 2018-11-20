4ca05837b2ab33bd8dbd3ae5eeab63e59ae27b8f
AlexeyCorr <alexeycorr@gmail.com>
2018-11-20 23:28:01 +0300

    Вынести модули по папкам

---
+++ js/screens/intro-screen.js
@@ -0,0 +1,19 @@
+import {render, showScreen} from './../util';
+import {greetingScreen} from './index';
+
+const template =
+  `<section class="intro">
+    <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+  </section>`;
+
+const element = render(template);
+
+const continueButton = element.querySelector(`.intro__asterisk`);
+
+continueButton.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  showScreen(greetingScreen);
+});
+
+export default element;
