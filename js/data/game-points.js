e0aa5762da6585c43bf2d2018ce7145850a96166
AlexeyCorr <alexeycorr@gmail.com>
2018-11-25 17:01:12 +0300

    Добавление тестов для функции подсчёта очков

---
+++ js/data/game-points.js
@@ -0,0 +1,24 @@
+import {InitialGame, Points} from './constants';
+
+const sumPoints = (answers, lives) => {
+  if (answers.length < InitialGame.QUESTIONS || lives < 1) {
+    return -1;
+  }
+
+  return answers
+    .filter(({isCorrect}) => isCorrect)
+    .reduce((points, {time}) => {
+      if (time < 10) {
+        points += Points.QUICK_ANSWER;
+        return points;
+      }
+      if (time > 20) {
+        points += Points.SLOW_ANSWER;
+        return points;
+      }
+      points += Points.NORMAL_ANSWER;
+      return points;
+    }, lives * Points.LIVES);
+};
+
+export {sumPoints};
