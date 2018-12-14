445d7af05d223e8ca3e037ef7c0d82b3605a1b3f
AlexeyCorr <alexeycorr@gmail.com>
2018-12-15 00:06:33 +0300

    Перенос логики мегания таймера в TimerView

--- js/screens/game-screen.js
+++ js/screens/game-screen.js
@@ -104,9 +104,6 @@ class GameScreen {
 
   updateTimer() {
     const timer = new TimerView(this.model.state);
-    if (this.model.state.time <= 5) {
-      timer.element.classList.add(`game__timer--blink`);
-    }
     this.header.replaceChild(timer.element, this.timer.element);
     this.timer = timer;
   }
